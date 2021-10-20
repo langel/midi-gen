e2dc987d6565f648582232699ec9938306c989bc
langel <knoxburry@gmail.com>
2021-10-20 08:50:10 -0400

    generating chords but not all identified

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ function rndint(max) {
 }
 
 console.log(chord.extended("Cmaj7"));
+let cminor = ['C', 'D#', 'G'];
+console.log(cminor);
+console.log(chord.detect(cminor));
 
 for (let i = 0; i < 10; i++) {
 	let tones = [];
+	let notes_temp = [...notes];
 	for (let j = 0; j < 4; j++) {
-		tones.push(notes[rndint(notes.length)]);
+		let index = rndint(notes_temp.length);
+		tones.push(notes_temp[index]);
+		notes_temp.splice(index, 1);
 	}
+	console.log();
 	console.log(tones);
-	console.log(chord.detect([tones]));
+	console.log(chord.detect(tones));
 }
